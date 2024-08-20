c1122f95debd0cb57f91723a8112d947a0a3fc0d
Benjamin Kaylor <blkaylor22@gmail.com>
2024-08-20 05:10:14 -0600

    tuned to perfection

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::num::NonZeroUsize;
 
+use log::{debug, error, info, log_enabled, Level};
+
 use anyhow::{Context, Result};
 
 use icy_metadata::{IcyHeaders, IcyMetadataReader, RequestIcyMetadata};
@@ -17,16 +19,17 @@ async fn spawn_radio() -> Result<()> {
 
     let client = Client::builder().request_icy_metadata().build()?;
 
-    let stream =
-        HttpStream::new(client, "https://stream.gensokyoradio.net/3".parse()?).await?;
+    let stream = HttpStream::new(client, "https://stream.gensokyoradio.net/1".parse()?).await?;
 
     let icy_headers = IcyHeaders::parse_from_headers(stream.headers());
-    println!("Icescast headers: {icy_headers:#?}\n");
-    println!("content type={:?}\n", stream.content_type());
+    if log_enabled!(Level::Debug) {
+        debug!("Icescast headers: {icy_headers:#?}\n");
+        debug!("content type={:?}\n", stream.content_type());
+    }
 
-    // buffer 5 seconds of audio
-    // bitrate (in kilobits) / bits per byte * bytes per kilobyte * 5 seconds
-    let prefetch_bytes = icy_headers.bitrate().unwrap() / 8 * 1024 * 10;
+    // buffer 20 seconds of audio
+    // bitrate (in kilobits) / bits per byte * bytes per kilobyte * 20 seconds
+    let prefetch_bytes = icy_headers.bitrate().unwrap() / 8 * 1024 * 20;
 
     let reader = StreamDownload::from_stream(
         stream,
@@ -38,13 +41,21 @@ async fn spawn_radio() -> Result<()> {
     )
     .await?;
 
-    sink.append(rodio::Decoder::new(IcyMetadataReader::new(
-        reader,
-        // Since we requested icy metadata, the metadata interval header should be present in the
-        // response. This will allow us to parse the metadata within the stream
-        icy_headers.metadata_interval(),
-        |metadata| println!("{metadata:#?}\n"),
-    )).context("Failed to decode audio from metadata")?);
+    if log_enabled!(Level::Debug) {
+        let size = std::mem::size_of_val(&reader);
+        debug!("Reader is: {}", size);
+    }
+
+    sink.append(
+        rodio::Decoder::new(IcyMetadataReader::new(
+            reader,
+            // Since we requested icy metadata, the metadata interval header should be present in the
+            // response. This will allow us to parse the metadata within the stream
+            icy_headers.metadata_interval(),
+            |metadata| println!("{metadata:#?}\n"),
+        ))
+        .context("Failed to decode audio from metadata")?,
+    );
 
     let handle = tokio::task::spawn_blocking(move || {
         sink.sleep_until_end();
@@ -54,14 +65,17 @@ async fn spawn_radio() -> Result<()> {
 }
 
 fn main() {
+    env_logger::init();
     loop {
         match spawn_radio() {
             Ok(_) => {
-                println!("Failed to recieve new Audio data from source.\nRestarting the connection.");
-            },
+                error!("Failed to recieve new Audio data from source.");
+                info!("Restarting the connection.");
+            }
             Err(_) => {
-                println!("Failed to create Decoder for audio stream.\nRestarting the connection.");
-            },
+                error!("Failed to create Decoder for audio stream.");
+                info!("Restarting the connection.");
+            }
         }
     }
 }
